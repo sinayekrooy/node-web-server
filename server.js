1c1b374d2af361b3b63e1737a77d6c71a7f92e26
Sina Yekrooy <sinayekrooy@gmail.com>
2018-07-24 14:53:19 +0430

    Initial Commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+// app.use((req, res, next) => {res.render('maintenance.hbs')})
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+    let now = new Date().toString()
+    let log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log)
+    fs.appendFile('serverLogs.log', log + '\n', (err) => {
+        if (err) console.log(err)
+    })
+
+    next()
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
